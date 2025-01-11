e461c450c8602d0b8dc7baef88fb261ae869c8a6
Kaique Ryan <kaique.github@gmail.com>
2025-01-10 22:25:11 -0300

    Alteração no algoritmo de Support Vector Computer para classificação preditiva de dados.

--- php-ia-projects/svc.php
+++ php-ia-projects/svc.php
@@ -7,14 +7,14 @@ use Phpml\Classification\SVC;
 $classifier = new SVC();
 
 $sizes = [
-    [30, 40, 50],
-    [60, 70, 80],
-    [90, 100, 110]
+    [30], 
+    [40],
+    [60]
 ];
 
 $labels = ['P', 'M', 'G'];
 
 $classifier->train($sizes, $labels);
-$size_code = $classifier->predict([40, 50]);
+$size_code = $classifier->predict([39]);
 
 print_r('Seu tamanho é: ' . $size_code);
