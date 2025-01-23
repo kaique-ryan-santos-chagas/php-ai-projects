bc33c1f064ae379c6322732c83bfa28449433fef
Kaique Ryan <kaique.github@gmail.com>
2025-01-22 21:39:44 -0300

    Conclusão do algoritmo de Kmeans para agrupamento de dados.

--- php-ia-projects/kmeans.php
+++ php-ia-projects/kmeans.php
@@ -10,7 +10,20 @@ $samples = [
     [2, 3], [2, 4], [7, 10], [8, 9]
 ];
 
-$result = $clustering->cluster($samples);
-$result = json_encode($result);
+$clusters = $clustering->cluster($samples);
+$clusters_organized = array();
 
-print_r($result);
+foreach($clusters as $cluster){
+
+    $cluster_organized = array();
+
+    foreach($cluster as $data_array){
+        foreach($data_array as $data){
+            array_push($cluster_organized, $data);
+        }
+    }
+
+    array_push($clusters_organized, $cluster_organized);
+}
+
+print_r($clusters_organized);
